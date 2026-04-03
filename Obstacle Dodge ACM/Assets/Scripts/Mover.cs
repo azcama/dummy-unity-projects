60f4924ac8e22c59e1a1a3b7632edf8e1873a7e4
azcama <azaharacm7@gmail.com>
2026-04-03 18:06:00 +0200

    Adding first part of my first project

---
+++ Obstacle Dodge ACM/Assets/Scripts/Mover.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float xValue = Input.GetAxis("Horizontal");
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical");
+        transform.Translate(xValue, yValue, zValue);
+    }
+}
